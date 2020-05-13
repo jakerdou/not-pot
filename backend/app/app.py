ac4eba8316cf2ac622a7bb991dd51058049d067a
James Robinson <44409757+jakerdou@users.noreply.github.com>
2020-05-13 16:09:08 -0500

    more docker files

--- backend/app/app.py
+++ backend/app/app.py
@@ -4,7 +4,7 @@ import cv2
 import numpy as np
 from flask import Flask
 from flask import request
-from tensorflow import keras
+# from tensorflow import keras
 from flask_cors import CORS
 # from flask_pymongo import PyMongo
 # from config import mongo_pswd
@@ -21,7 +21,7 @@ CORS(app)
 #
 # db = mongo.db
 
-model = keras.models.load_model("model.h5")
+# model = keras.models.load_model("model.h5")
 
 
 @app.route('/')
@@ -38,17 +38,18 @@ def get_model():
         npimg = np.fromstring(filestr, np.uint8)
         img = cv2.imdecode(npimg, cv2.IMREAD_COLOR)
 
-        percent_pot = str(eval_image(img, model))
+        # percent_pot = str(eval_image(img, model))
+        percent_pot = "0.984"
 
         # we kinda don't need the database, just to read the file taht's passed in
         # mongo.save_file(file_name, my_img_file)
         return percent_pot
 
 
-def eval_image(img, model):
-    IMG_SIZE = 125
-    RGB_MAX_VAL = 255.0
-    img = cv2.resize(img, (IMG_SIZE, IMG_SIZE))
-    img = img / RGB_MAX_VAL
-    result = model.predict([[img]])
-    return result[0][0]
+# def eval_image(img, model):
+#     IMG_SIZE = 125
+#     RGB_MAX_VAL = 255.0
+#     img = cv2.resize(img, (IMG_SIZE, IMG_SIZE))
+#     img = img / RGB_MAX_VAL
+#     result = model.predict([[img]])
+#     return result[0][0]
