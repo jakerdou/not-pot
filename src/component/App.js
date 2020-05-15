b44a35a68a895d1838b53010c1ad79fead98f035
James Robinson <44409757+jakerdou@users.noreply.github.com>
2020-05-15 17:41:32 -0500

    todos

--- src/component/App.js
+++ src/component/App.js
@@ -10,7 +10,7 @@ import 'antd/dist/antd.css';
 
 const { Header, Content, Footer } = Layout;
 
-// TODO: remove config from git repo
+// TODO: remove config from git repo, make serve over https, maybe do through aws
 
 class App extends React.Component{
     render(){
